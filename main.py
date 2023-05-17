dc0d475455b1a1ff4a9e53ed2d31b0e4e5e72042
Sparab16 <shreyas.parab16@gmail.com>
2023-05-17 16:58:44 +0530

    Refactor: Added print statement after successfull completion of Authentication requests

--- main.py
+++
@@ -1,5 +0,0 @@
-from services.authorization.spotify_authenticator import SpotifyAuthenticator
-from services.player.spotify_player import SpotifyPlayer
-
-# SpotifyAuthenticator.get_refresh_token()
-SpotifyPlayer.get_playback_state()
